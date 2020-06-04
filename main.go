795dc76a7fbedc47e463e254b2a17ddfef984c1f
Xenia <lommy101@gmail.com>
2020-06-04 15:58:13 +0900

    new commit

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Xenia101/ytdl"
+)
+
 func main() {
-	URL := "https://www.youtube.com/watch?v=VEDLnex6YeQ&list=RDVEDLnex6YeQ&start_radio=1"
-	DownloadFromURL(URL)
+	vid, err := ytdl.GetVideoInfo(context.Background(), "https://www.youtube.com/watch?v=WkVvG4QTO9M")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	file, _ := os.Create(vid.Title + ".mp4")
+	defer file.Close()
+	ytdl.DefaultClient.Download(context.Background(), vid, vid.Formats.Worst(ytdl.FormatResolutionKey)[0], file)
 }

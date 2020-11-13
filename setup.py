86ca697c66bd6c43dbc531906ea5fd2d49d76d91
XENIA <lommy101@gmail.com>
2020-11-13 10:17:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from cx_Freeze import setup, Executable
 import sys
 
-buildOptions = dict(packages = ["queue", "qtmodern.windows", "qtmodern.styles"], 
-                    excludes = [],
-                    includes = ["guiWindow", "checkpath"],
+buildOptions = dict(packages      = ["queue", "qtmodern.windows", "qtmodern.styles"], 
+                    excludes      = [],
+                    includes      = ["guiWindow", "checkpath"],
                     include_files = ["style.qss", "gui.ini"]
                     )
 
@@ -14,10 +14,10 @@ if sys.platform == "win32":
 exe = [Executable("main.py", base=base, icon = "Xentube.ico")]
 
 setup(
-    name='Xentube',
-    version = '1.0',
-    author = "XENIA",
+    name        = 'Xentube',
+    version     = '1.0',
+    author      = "XENIA",
     description = "downloads, plays, and collects YouTube videos [Python & Qt]",
-    options = dict(build_exe = buildOptions),
+    options     = dict(build_exe = buildOptions),
     executables = exe
 )
